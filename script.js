657db9fdd253393ffbd86e3eff9b869d79dafcc8
Viktor <slavik00757@gmail.com>
2023-10-26 09:37:22 +0200

    Start Menu is done. Create lobby and join lobby is not ready.

---
+++ script.js
@@ -0,0 +1,21 @@
+// const container = document.createElement('div')
+// container.style.width = "100vw";
+// container.style.height = "100vh";
+// container.style.backgroundColor = "black";
+// container.style.position = "fixed";
+// container.style.top = "100vh"
+
+
+
+
+function CreateLobbyStart(){
+    for (let index = -100; index < 1; index++) {
+            document.getElementsByClassName('menu-container')[0].style.top = `${index}vh`;
+            
+
+        }
+}
+
+function JoinLobbyStart(){
+
+}
