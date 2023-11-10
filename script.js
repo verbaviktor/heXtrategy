d751b19ec7654308c1068ff17dc3af7e34adf92a
Viktor <slavik00757@gmail.com>
2023-11-10 09:37:46 +0100

    Ujra minden, header 70% kesz

--- script.js
+++
@@ -1,4 +0,0 @@
-import { CreateLobbyStart, JoinLobbyStart } from "./Create_Join_Render.js";
-
-document.getElementById("CreateButton").addEventListener("click" , CreateLobbyStart)
-document.getElementById("JoinButton").addEventListener("click" , JoinLobbyStart)
