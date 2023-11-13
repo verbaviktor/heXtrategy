eefe95811d44bc7e1275e7e6538b2d52b129b8ba
abos.gergo <gergo.abos@gmail.com>
2023-11-13 15:57:11 +0200

    Finished click detection Reworked mapgen

--- scripts/camera.js
+++ scripts/camera.js
@@ -1,3 +1,5 @@
+import { ctx, map } from "./script.js";
+
 const yVector = [Math.cos(Math.PI / 3), Math.sin(Math.PI / 3)];
 const xVector = [1, 0];
 
@@ -5,7 +7,7 @@ export class Camera {
     constructor() {
         this.x = 0;
         this.y = 0;
-        this.zoom = 1;
+        this.tileSize = 50;
         document.addEventListener('keypress', (e) => {
             switch (e.key) {
                 case 'w':
@@ -26,17 +28,41 @@ export class Camera {
         })
     }
 
-    screenToHex(screenX, screenY, mapRadius) {
-        let hexY = (screenY + this.y) / 50 / yVector[1];
-        let xOffset = Math.max(mapRadius - hexY, 1);
-        let hexX = (screenX + this.x) / 50 - hexY * yVector[0] - xOffset;
-        return [hexX, hexY];
+    screenToHex(screenX, screenY) {
+        let world_screenY = screenY + this.y
+        let world_screenX = screenX + this.x
+
+        let hexYGuess = (world_screenY / Math.cos(Math.PI / 6) - this.tileSize / 2) / (yVector[1] * this.tileSize);
+        let clickedRows = [Math.floor(hexYGuess), Math.ceil(hexYGuess)]
+
+        let min_distance_squared = 100000000000000000000000;
+        let clicked_coordinates = null
+        for (const y of clickedRows) {
+            const tilesInRow = map.getTilesInRow(y)
+            if (!tilesInRow) {
+                continue
+            }
+            for (const hex of tilesInRow) {
+                let hexScreenPosition = this.hexToScreen(hex.x, hex.y)
+                let vectorFromClickToHex = [hexScreenPosition[0] - screenX, hexScreenPosition[1] - screenY]
+                const distance_squared = vectorFromClickToHex[0] ** 2 + vectorFromClickToHex[1] ** 2
+                if (distance_squared < min_distance_squared) {
+                    clicked_coordinates = [hex.x, hex.y]
+                    min_distance_squared = distance_squared
+                }
+            }
+        };
+
+        return clicked_coordinates;
     }
 
-    hexToScreen(hexX, hexY, mapRadius) {
-        let xOffset = Math.max(mapRadius - hexY, 1);
-        let screenX = hexY * yVector[0] + (hexX + xOffset) * xVector[0];
-        let screenY = hexY * yVector[1];
-        return [screenX * 50 - this.x, screenY * 50 - this.y];
+    hexToScreen(hexX, hexY) {
+        let xOffset = Math.max(map.radius - hexY, 1);
+        let worldX = hexY * yVector[0] + (hexX + xOffset) * xVector[0];
+        let worldY = hexY * yVector[1];
+        return [
+            (worldX * this.tileSize + (this.tileSize / 2)) * Math.cos(Math.PI / 6) - this.x,
+            (worldY * this.tileSize + (this.tileSize / 2)) * Math.cos(Math.PI / 6) - this.y
+        ];
     }
 }
--- scripts/script.js
+++ scripts/script.js
@@ -3,53 +3,15 @@ import { Map } from "./map.js";
 import { Player } from "./player.js";
 
 let canvas = document.querySelector('#gamecanvas');
-var ctx = canvas.getContext('2d');
-var gameobjects = [];
-gameobjects.push(new Map(10, [new Player(), new Player()]));
-let camera = new Camera();
+export let ctx = canvas.getContext('2d');
+export let map = new Map(10, [new Player(), new Player()]);
+export let camera = new Camera();
 
 var lastTime = 0;
 var deltaTime = 0;
 
-function getCursorPosition(canvas, event) {
-    const rect = canvas.getBoundingClientRect()
-    const x = Math.round(event.clientX - rect.left);                    //screenCoordinates
-    const y = Math.round(event.clientY - rect.top);
-
-    const diameter = gameobjects[0].tileSize/Math.cos(Math.PI / 6);     //diameter of one tile in screenCoordinates
-    const rowHeight = diameter * 3/4;                                   //hexagon diameter without the quarter at the bottom
-
-    const row1Index = Math.floor(y/rowHeight) - 1;                      //index of the row above the clicked point
-    const row2Index = Math.floor(y/rowHeight)                           //index of the row where the clicked point is
-    const row1Center = row1Index * rowHeight + diameter/2;              //horizontal center line of the row in screenCoordinates
-    const row2Center = row2Index * rowHeight + diameter/2;
-
-    const offset1 = Math.abs(gameobjects[0].radius - 1 - row1Index) * 25 + 278;
-    const offset2 = Math.abs(gameobjects[0].radius - 1 - row2Index) * 25 + 278;
-    let x1 = x - offset1;
-    let x2 = x - offset2;
-
-    const col1Index = Math.floor(x1/gameobjects[0].tileSize);           //index of the column where the clicked x point is in the first row
-    const col2Index = Math.floor(x2/gameobjects[0].tileSize);           //index of the column where the clicked x point is in the second row
-    const col1Center = col1Index * gameobjects[0].tileSize + gameobjects[0].tileSize/2 + offset1;   //vertical center line of the first column 
-    const col2Center = col2Index * gameobjects[0].tileSize + gameobjects[0].tileSize/2 + offset2;
-
-    const xDistance1 = Math.abs(x - col1Center);                        //distance between the clicked point and the vertical center of the first tile
-    const xDistance2 = Math.abs(x - col2Center);
-    const yDistance1 = Math.abs(y - row1Center);                        //distance between the clicked point and the horizontal center of the first tile
-    const yDistance2 = Math.abs(y - row2Center);
-    const distance1 = xDistance1 + yDistance1;                          //total distance between the clicked point and the center point of the first tile
-    const distance2 = xDistance2 + yDistance2;
-    
-    if (distance1 > distance2) {
-        console.log(gameobjects[0].matrix[row2Index][col2Index]);
-    }
-    else{
-        console.log(gameobjects[0].matrix[row1Index][col2Index]);
-    }
-}
 canvas.addEventListener('mousedown', function(e) {
-    getCursorPosition(canvas, e)
+    console.log(camera.screenToHex(e.clientX, e.clientY))
 });
 
 function gameLoop(timestamp) {
@@ -59,9 +21,8 @@ function gameLoop(timestamp) {
     ctx.rect(0, 0, canvas.width, canvas.height);
     ctx.fillStyle = "#3F3F3F";
     ctx.fill();
-    for (const gameobject of gameobjects) {
-        gameobject.render(ctx, camera);
-    }
+
+    map.render()
 
 
     lastTime = timestamp;
